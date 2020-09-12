8f64cea03758d822db2b9c360bdf9412c21cf775
marutham21 <66676872+marutham21@users.noreply.github.com>
2020-09-12 10:10:23 +0530

    Created product.cpp
    This file provides the product of two numbers

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    float num1, num2, product;
+    cout << "Enter two Numbers:\n";
+    cin >> num1 >>num2 ;
+    product = num1*num2;
+    cout << "Product = " << product;
+    return 0;
+}
